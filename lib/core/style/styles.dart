957f0d37d16027acf7255e187fba2b40dfb9d2bb
EslamFares <eslamfareselazb@gmail.com>
2023-11-20 23:19:40 +0200

    add app strings

---
+++ lib/core/style/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  TextStyle font14Bold =
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
+}
--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,3 +1,85 @@
 abstract class AppStrings {
-  static const String appName = 'Ra7alh';
+  static const String appName = "Ra7alh",
+      //?On Boarding Strings
+      skip = "Skip",
+      next = "Next",
+      createAccount = "Create Account",
+      loginNow = "Login Now",
+      //?splash screns heads & body
+      splashTextH1 = "Explore The history with \nDalel in a smart way",
+      splashTextB1 =
+          "Using our app's history libraries \nyou can find many historical periods ",
+      splashTextH2 = "From every place \non earth",
+      splashTextB2 =
+          "A big variety of ancient places \nfrom all over the world",
+      splashTextH3 = "Using modern AI technology /nfor better user experience",
+      splashTextB3 =
+          "AI provide recommendations and helps \nyou to continue the search journey",
+      splashTextH4 = "Ra7alh App wishes you a happy trip",
+      splashTextBa =
+          "We hope you enjoy the trip in Egypt, \nthe Mother of the World",
+      splashTextH5 = "Ra7alh App",
+      splashTextB5 = "Let's go ....",
+
+      //?Auth Strings
+      welcome = "Welcome!",
+      welcomeBack = "Welcome Back!",
+      fristName = "First Name",
+      lastName = "Last Name",
+      emailAddress = "Email Address",
+      password = "Password",
+      iHaveAgreeToOur = "I have agree to our ",
+      termsAndCondition = "Terms and Condition",
+      signUp = "Sign Up",
+      signIn = "Sign In",
+      alreadyHaveAnAccount = "Already have an account ? ",
+      dontHaveAnAccount = "Don’t have an account ? ",
+      forgotPassword = "Forgot Password ?",
+      //!Home Strings
+      recommendations = "Recommendations",
+      about = "About",
+      wars = "Wars",
+      //?Search Strings
+      searchDeepIntoTheHistory = "Search deep into the history",
+      recent = "Recent",
+      //?Profile Strings
+      profile = "Profile",
+      account = "Account",
+      editProfile = "Edit Profile",
+      notification = "Notification",
+      general = "General",
+      settings = "Settings",
+      security = "Security",
+      privacyPolicy = "Privacy Policy",
+      logOut = "Log Out",
+      //?Bazar
+      bazar = "Bazar",
+      historicalPeriods = "Historical periods",
+      historicalCharacters = "Historical Characters",
+      historyBooks = "History Books",
+      historicalSouvenirs = "Historical Souvenirs",
+      //?Cart
+      myCart = "My Cart",
+      totalPayment = "Total Payment",
+      checkoutNow = "Checkout Now",
+      //?Payment Strings
+      checkoutScreen = "Checkout Screen",
+      deliveryAddress = "Delivery Address",
+      change = "Change",
+      items = "1 items",
+      selectedProduct = "Selected Product",
+      paymentMethod = "Payment Method",
+      confirmPayment = "Confirm Payment",
+      orderSuccess = "Order Success",
+      thankYou =
+          "Thank you for your order here and your package will be sent to your address very quickly and fast good product",
+      backToHome = "Back To Home",
+      //?Forgot password
+      forgotPasswordPage = "Forgot Password",
+      sendResetPasswordLink = "Send Reset Passwrod Link",
+      verificationNow = "Verification Now",
+      resendCode = "Resend Code",
+      verifyAccount = "Verify Account",
+      enter4DigitCodeWeHaveSentTo = "Enter 4 digit code we have sent to ",
+      haventReceivedVerificationCode = "Haven't received verification code?";
 }
