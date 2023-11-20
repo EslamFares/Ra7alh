08939b2ba5941e2a8afc0870758144bcba52a1be
EslamFares <eslamfareselazb@gmail.com>
2023-11-20 22:19:23 +0200

    Fist Load

---
+++ android/app/src/main/kotlin/com/example/ra7alh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eslamfares.ra7alh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/style/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorApp {
+  static const Color colorBLue = Color(0xff407BFF);
+  static const Color colorGray = Color(0xff455A64);
+}
