571b223a5dc9486f5821dac7b706ce1d624d85c1
EslamFares <eslamfareselazb@gmail.com>
2023-11-20 22:37:20 +0200

    add app assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,9 @@
+abstract class AppAssets {
+  static const String splash1 = 'assets/img/1.png';
+  static const String splash2 = 'assets/img/2.png';
+  static const String splash3 = 'assets/img/3.png';
+  static const String splash4 = 'assets/img/4.png';
+  static const String splash5 = 'assets/img/5.png';
+  static const String icon = 'assets/icon/icon.png';
+  static const String iconSmall = 'assets/icon/icon_small.png';
+}
--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ColorApp {
+class AppColors {
   static const Color colorBLue = Color(0xff407BFF);
   static const Color colorGray = Color(0xff455A64);
 }
