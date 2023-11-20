e5c6b8fbad04a276d6f5b08e3b75d5dd161d2928
EslamFares <eslamfareselazb@gmail.com>
2023-11-20 22:45:48 +0200

    app colors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
-  static const Color colorBLue = Color(0xff407BFF);
-  static const Color colorGray = Color(0xff455A64);
+abstract class AppColors {
+  static const Color pcBlue = Color(0xff407BFF);
+  static const Color pcBlue2 = Color(0xff90AAE5);
+  static const Color colorGray = Color(0xffEBEBEB);
+  static const Color colorGray2 = Color(0xff455A64);
+  static const Color colorGray3 = Color(0xff37474F);
+  static const Color colorBlack = Color(0xff263238);
+  static const Color colorBlack2 = Color(0xff000000);
 }
